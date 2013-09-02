1d16d306a0f0b6e59b59e810fe26f27203b829c2
Simon Luijk <simon@simonluijk.com>
2013-09-02 16:22:22 +0200

    Use relative urls in less
    Signed-off-by: Simon Luijk <simon@simonluijk.com>

--- watch_media.sh
+++ watch_media.sh
@@ -7,10 +7,10 @@ coffee -cmw *.coffee &
 
 cd $home
 less_file="static/css/core.less";
-lessc -x --verbose $less_file ${less_file%.*}.min.css;
+lessc -ru -x --verbose $less_file ${less_file%.*}.min.css;
 
 while true; do
     files=`find -name "*.less"`;
     inotifywait -qe modify $files;
-    lessc -x --verbose $less_file ${less_file%.*}.min.css;
+    lessc -ru -x --verbose $less_file ${less_file%.*}.min.css;
 done;
